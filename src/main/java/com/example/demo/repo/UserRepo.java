33f726a606cd30ccfc38800b8f5c19842b40f963
AZIAD711 <aziad711@gmail.com>
2025-09-15 11:29:15 +0300

    Update UserRepo.java

--- src/main/java/com/example/demo/repo/UserRepo.java
+++ src/main/java/com/example/demo/repo/UserRepo.java
@@ -1,8 +1,9 @@
 package com.example.demo.repo;
-import com.example.demo.eniity.UserEntity;
+
+import com.example.demo.entity.UserEntity;   
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
+
 @Repository
-public interface UserRepo extends JpaRepository<UserEntity,Long> {
+public interface UserRepo extends JpaRepository<UserEntity, Long> {
 }
